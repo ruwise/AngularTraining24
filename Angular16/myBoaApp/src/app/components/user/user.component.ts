74c53639a721d46784684abb4b4fad317247bbb3
ruwise <ruwise@outlook.com>
2024-04-12 10:25:13 +0530

    forth commit

--- Angular16/myBoaApp/src/app/components/user/user.component.ts
+++ Angular16/myBoaApp/src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,23 +8,96 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-  getData: any[] | undefined;
-
+  userList: User[] | undefined;
+  defaultUser: User = new User("1000", "", "", "");
+  buttonName = "Add User";
+  isEdit: boolean = false;
+  id: number = 1000;
+  showUsers: boolean = false;
   constructor(private _httpService: UserService) { }
 
   ngOnInit(): void {
+    this.getUserDetails();
   }
 
   // Fetch user details
-  getUserDetails(){
-
-
-    this._httpService.getUsers().subscribe((res :any[])=>{
-
-         console.log("get service response-->" +res);
-         this.getData=res;
-
+  getUserDetails() {
+    this._httpService.getAllUsers().subscribe((res: User[]) => {
+      console.log("get service response-->" + res);
+      this.userList = res;
+      this.userList.sort((a, b) => Number(a.id) - Number(b.id));
     })
+  }
+  showAllUsers(){
+    this.getUserDetails();
+    this.showUsers = true;
+  }
 
-}
+  // add new user
+  createUser() {
+    if (this.defaultUser.first_name.trim().length == 0
+      || this.defaultUser.last_name.trim().length == 0
+      || this.defaultUser.email.trim().length == 0) {
+      return;
+    }
+    if (this.isEdit) {
+      this.updateUser();
+    } else {
+      if(this.userList != undefined && this.userList.length > 0){
+        this.id = Number(this.userList[this.userList.length-1].id);
+      }
+      this.id += 1;
+      let user = new User(this.id.toString(), this.defaultUser.first_name, this.defaultUser.last_name, this.defaultUser.email);
+      this._httpService.createNewUSer(user).subscribe((res: User) => {
+        console.log("User created!");
+        console.log(JSON.stringify(res));
+        this.getUserDetails();
+      });
+    }
+    this.clearForm();
+  }
+
+  // updae the selected user
+  updateUser(){
+    this._httpService.updateUser(this.defaultUser).subscribe((res: User) => {
+      console.log("User updated!");
+      console.log(JSON.stringify(res));
+      this.getUserDetails();
+    });
+  }
+
+  // edit user details based on id
+  editUser(user: User) {
+      this.defaultUser.id = user.id;
+      this.defaultUser.first_name = user.first_name;
+      this.defaultUser.last_name = user.last_name;
+      this.defaultUser.email = user.email;
+      this.isEdit = true;
+      this.buttonName = "Update User";
+
+  }
+
+  // delete user by id
+  deleteUser(id: string) {
+    this._httpService.deleteUser(id).subscribe((res: User) => {
+      console.log("User updated!");
+      console.log(JSON.stringify(res));
+      this.getUserDetails();
+    });
+  }
+
+  // hide user list and clear the form fields
+  clearUserList() {
+    this.showUsers = false;
+    this.clearForm();
+  }
+
+  // clear form fields
+  clearForm() {
+    this.defaultUser.first_name = "";
+    this.defaultUser.last_name = "";
+    this.defaultUser.email = "";
+    this.buttonName = "Add User";
+    this.isEdit = false;
+  }
 }
--- Angular16/myBoaApp/src/app/services/user.service.ts
+++ Angular16/myBoaApp/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +18,25 @@ export class UserService {
     this.httpClient = httpClient;
   }
 
+  //   "/users/delete/:id": "/users/:id"
+
   // Implement the getUsers Service method  // GET IMPL 
-  getUsers(): Observable<any> {
-
+  getAllUsers(): Observable<any> {
     let getUserUrl: string = 'http://localhost:7001/users';
-    return this.httpClient.get(getUserUrl, { headers: this.headers })
+    return this.httpClient.get(getUserUrl, { headers: this.headers });
+  }
 
+  createNewUSer(user: User): Observable<User> {
+    let creatUserUrl: string = 'http://localhost:7001/users/';
+    return this.httpClient.post<User>(creatUserUrl, user, {headers: this.headers});
+  }
+
+  updateUser(user: User): Observable<User> {
+    let creatUserUrl: string = 'http://localhost:7001/users/'+user.id;
+    return this.httpClient.put<User>(creatUserUrl, user, {headers: this.headers});
+  }
+  deleteUser(id: string): Observable<User> {
+    let creatUserUrl: string = 'http://localhost:7001/users/'+id;
+    return this.httpClient.delete<User>(creatUserUrl, {headers: this.headers});
   }
 }
